5fe179797304412e5f69ac17b8abd2c435130a55
Lucas <114083832+Lucca7r@users.noreply.github.com>
2025-05-13 18:08:03 -0300

    chaat criado

---
+++ src/styles/ChatScreen.style.ts
@@ -0,0 +1,38 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  chat: {
+    padding: 16,
+    flexGrow: 1,
+    justifyContent: 'flex-end',
+  },
+  message: {
+    padding: 10,
+    borderRadius: 12,
+    marginBottom: 10,
+    maxWidth: '80%',
+  },
+  inputWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 25,
+    borderWidth: 1,
+    paddingHorizontal: 12,
+    height: 45,
+    marginHorizontal: 16,
+  },
+  inputInside: {
+    flex: 1,
+    fontSize: 16,
+  },
+  sendInlineButton: {
+    marginLeft: 8,
+    backgroundColor: '#3C9A5B',
+    paddingHorizontal: 14,
+    paddingVertical: 8,
+    borderRadius: 25,
+  },
+});
