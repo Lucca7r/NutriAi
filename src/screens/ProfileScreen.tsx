4baab2406986218f14b9c905679a6459b8d1967f
Luan_Medrado8 <132151609+LuanMedrado8@users.noreply.github.com>
2025-06-05 21:46:06 +0000

    botão Sair da Conta funcional

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -8,26 +8,36 @@ import {
 } from 'react-native';
 import { useTheme, useThemeColors } from '../context/ThemeContext';
 import ProfileHeader from '../components/ProfileHeader';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../@types/navigation';
 
 export default function ProfileScreen() {
   const { theme, toggleTheme } = useTheme();
   const colors = useThemeColors();
   const isDark = theme === 'dark';
 
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+
+  const handleLogout = () => {
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'Login' }],
+    });
+  };
+
   return (
     <View style={[styles.container, { backgroundColor: colors.background }]}>
       <ProfileHeader />
-
       <View style={styles.section}>
         <Text style={[styles.label, { color: colors.text }]}>Modo Escuro</Text>
         <Switch value={isDark} onValueChange={toggleTheme} />
       </View>
-
       <View style={styles.section}>
         <TouchableOpacity>
           <Text style={[styles.link, { color: colors.icon }]}>Editar Perfil</Text>
         </TouchableOpacity>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={handleLogout}>
           <Text style={[styles.link, { color: colors.icon }]}>Sair da Conta</Text>
         </TouchableOpacity>
       </View>
