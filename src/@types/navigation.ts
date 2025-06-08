a065b1fec7db05c85a57998451436ea57628780d
Lucas <114083832+Lucca7r@users.noreply.github.com>
2025-06-08 19:17:48 -0300

    ajustes

--- src/@types/navigation.ts
+++ src/@types/navigation.ts
@@ -1,10 +1,10 @@
 // src/types/navigation.ts
 export type RootStackParamList = {
   Cadastro: undefined;
+  Login: undefined;
   Home: undefined;
   Chat: undefined;
   Profile: undefined;
   Favorites: undefined;
   Main: undefined; 
-  Login?: undefined;
 };
